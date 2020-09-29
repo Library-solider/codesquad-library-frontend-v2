65d1e4f816496956359758d636415335e310a1b1
choijangho <jhchoi1115@gmail.com>
2020-09-30 03:25:01 +0900

    Style : styled-component DefaultTheme 재정의

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,22 @@
+// import original module declarations
+import "styled-components";
+
+// and extend them!
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      basicFont: string;
+
+      black: string;
+      white: string;
+      red: string;
+      blue: string;
+      green: string;
+      gray: string;
+    };
+
+    boxShadow: {
+      base: string;
+    };
+  }
+}
