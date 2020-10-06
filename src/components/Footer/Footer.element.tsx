1344288dd3cc193d05120da988fddf5af178a9b9
choijangho <jhchoi1115@gmail.com>
2020-10-07 04:13:11 +0900

    Feat : Footer Component 구현

---
+++ src/components/Footer/Footer.element.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const FooterWrapper = styled.div`
+  width: 100%;
+  height: 100px;
+  padding-top: 100px;
+  font-size: 14px;
+
+  display: flex;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  flex-direction: column;
+  line-height: 1.8rem;
+`;
+
+export default FooterWrapper;
