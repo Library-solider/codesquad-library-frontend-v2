5da0476c185d7a7b150b508c1213ab87f967f3d3
choijangho <jhchoi1115@gmail.com>
2020-10-08 07:36:33 +0900

    Feat : BookCard 폴더로 분리

--- src/components/BookCard/BookCard.tsx
+++ src/components/BookCard/BookCard.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { MainBookCardWrapper } from "./MainBook.element";
-import { IMainBookCard } from "../../api/mainBook";
+import BookCardWrapper from "./BookCard.element";
+import { IBookCard } from "../../api/bookCard";
 
-function MainBookCard({ bookId, imageUrl, title, author }: IMainBookCard) {
+function BookCard({ bookId, imageUrl, title, author }: IBookCard) {
   return (
     <Link to={`/books/${bookId}`}>
-      <MainBookCardWrapper>
+      <BookCardWrapper>
         <img src={imageUrl} alt="book wrapper" />
         <p className="book-title">{title}</p>
         <p className="book-author">{author}</p>
-      </MainBookCardWrapper>
+      </BookCardWrapper>
     </Link>
   );
 }
 
-export default MainBookCard;
+export default BookCard;
