05c8be42811cfffbaca4780c0584b03e9bb28c8b
choijangho <jhchoi1115@gmail.com>
2020-10-08 07:37:07 +0900

    Feat : Loading Component 추가

---
+++ src/components/Loading/Loading.element.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const LoadingWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+
+  img {
+    width: 100px;
+    height: 100px;
+    padding: 24px;
+  }
+`;
+
+export default LoadingWrapper;
