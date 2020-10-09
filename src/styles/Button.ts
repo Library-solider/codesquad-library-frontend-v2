63e31ad0ef0714f90dc9875769dc47f2ec31f9d8
choijangho <jhchoi1115@gmail.com>
2020-10-09 10:55:57 +0900

    Style : Button Component 추가

--- src/styles/Button.ts
+++ src/styles/Button.ts
@@ -1,1 +1,23 @@
-import stlyed from "styled-components";
+import styled from "styled-components";
+
+// interface IStyleButton {
+//   fillColor: string;
+//   textColor: string;
+// }
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  border-radius: 4px;
+  height: 32px;
+  padding: 12px;
+  background-color: ${({ theme: { colors } }) => colors.green};
+  color: ${({ theme: { colors } }) => colors.white};
+
+  :hover {
+    transition: 0.3s;
+    opacity: 0.8;
+  }
+`;
+
+export const DefaultButton = styled.button``;
